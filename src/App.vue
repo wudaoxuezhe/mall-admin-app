8d6bc4d994a174607cdf96c4fa9c7b37388238eb
wudaoxuezhe <874192735@qq.com>
2021-03-24 17:43:57 +0800

    第一次初始化与提交

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,26 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <a-config-provider :locale="zh_CN">
+    <div id="app">
+      <router-view></router-view>
     </div>
-    <router-view/>
-  </div>
+  </a-config-provider>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import zh_CN from 'ant-design-vue/lib/locale-provider/zh_CN';
 
-#nav {
-  padding: 30px;
+export default {
+  name: 'app',
+  data() {
+    return {
+      zh_CN,
+    };
+  },
+  components: {
+  },
+};
+</script>
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+<style>
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
