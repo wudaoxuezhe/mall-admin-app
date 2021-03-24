451affbc437e473fe01a15363364461d2b27ff75
wudaoxuezhe <874192735@qq.com>
2021-03-24 23:22:29 +0800

    delete dist

--- dist/js/about.f5cf94f6.js
+++
@@ -1,6 +0,0 @@
-(window.webpackJsonp = window.webpackJsonp || []).push([['about'], {
-  f820(t, e, n) {
-    n.r(e); const a = function () { const t = this; const e = t.$createElement; t._self._c; return t._m(0); }; const s = [function () { const t = this; const e = t.$createElement; const n = t._self._c || e; return n('div', { staticClass: 'about' }, [n('h1', [t._v('This is an about page')])]); }]; const u = n('2877'); const c = {}; const i = Object(u.a)(c, a, s, !1, null, null, null); e.default = i.exports;
-  },
-}]);
-// # sourceMappingURL=about.f5cf94f6.js.map
