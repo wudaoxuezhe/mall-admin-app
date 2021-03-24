6f28c5fc83dac1b638a3bd6d46b47973e07332a9
wudaoxuezhe <874192735@qq.com>
2021-03-24 21:29:09 +0800

    整理布局代码

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,18 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    // 用于切换菜单的折叠状态，true 代表折叠， false 代表不折叠
+    collapsed: false,
   },
   mutations: {
+    changeCollapsed(state) {
+      state.collapsed = !state.collapsed;
+    },
   },
   actions: {
+    changeCollapsed({ commit }) {
+      commit('changeCollapsed');
+    },
   },
   modules: {
   },
